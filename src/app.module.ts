4c04384abf29c128511da5bc8a5cd55fdf768a24
坂井佑 <yusakai@sakaiyuunoMacBook-Air.local>
2023-11-24 21:45:09 +0900

    28_ClassValidator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
